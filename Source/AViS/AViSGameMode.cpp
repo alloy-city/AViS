de9500146cc98956487fc2f3dfe91fd2fdae0919
prof barba <bernardodesousa@gmail.com>
2019-08-09 13:52:16 -0300

    UE4

---
+++ Source/AViS/AViSGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "AViSGameMode.h"
+#include "AViSHUD.h"
+#include "AViSCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AAViSGameMode::AAViSGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AAViSHUD::StaticClass();
+}
---
+++ Source/AViS/MovingPlatform.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AVIS_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+};
